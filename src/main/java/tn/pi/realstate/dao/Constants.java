a3279fc732ac98af01ee2aa79ee48db9f309b1cc
ski1a <bilelzr8@gmail.com>
2024-02-25 22:14:58 +0100

    making email unique

---
+++ src/main/java/tn/pi/realstate/dao/Constants.java
@@ -0,0 +1,10 @@
+package tn.pi.realstate.dao;
+
+public class Constants {
+
+
+    private Constants(){
+
+    }
+    public static final String USER_EXIST = "User with the given email already exists.";
+}
--- src/main/java/tn/pi/realstate/security/AuthenticationServiceImpl.java
+++ src/main/java/tn/pi/realstate/security/AuthenticationServiceImpl.java
@@ -10,7 +10,10 @@ import tn.pi.realstate.dao.request.SigninRequest;
 import tn.pi.realstate.dao.response.JwtAuthenticationResponse;
 import tn.pi.realstate.entities.Role;
 import tn.pi.realstate.entities.User;
+import tn.pi.realstate.exceptions.UserAlreadyExistsException;
 import tn.pi.realstate.repositories.UserRepository;
+
+import static tn.pi.realstate.dao.Constants.USER_EXIST;
 
 @Service
 @RequiredArgsConstructor
@@ -23,13 +26,30 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public JwtAuthenticationResponse signup(SignUpRequest request) {
-        var user = User.builder().firstName(request.getFirstName()).lastName(request.getLastName())
-                .email(request.getEmail()).password(passwordEncoder.encode(request.getPassword()))
-                .role(Role.USER).build();
+        // Check if a user with the given email already exists
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            // User with the given email already exists, handle accordingly (throw an exception, return an error response, etc.)
+            // For simplicity, let's assume you want to throw an exception
+            throw new UserAlreadyExistsException(USER_EXIST);
+        }
+        // User with the given email doesn't exist, proceed to create a new user
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
         userRepository.save(user);
+
+        // Generate JWT token for the newly created user
         var jwt = jwtService.generateToken(user);
+
+        // Return the JWT token in the response
         return JwtAuthenticationResponse.builder().token(jwt).build();
     }
+
 
     @Override
     public JwtAuthenticationResponse signin(SigninRequest request) {
